52abafa45bbb7c77e65d4843abbc8a8bbfed53fe
lastrider <l.leonowicz@gmail.com>
2020-03-31 21:56:34 +0200

    Zadanie 1

--- src/main/java/com/kodilla/Application.java
+++ src/main/java/com/kodilla/Application.java
@@ -1,4 +1,30 @@
 package com.kodilla;
 
 public class Application {
+    public static void main(String[] args) {
+        int binNumber = 10111000;
+        new ChangeBinToDec().swap(binNumber);
+    }
+}
+
+class ChangeBinToDec {
+
+    public void swap(Integer binNumber) {
+        int exponent = 0;
+        int intNumber = 0;
+
+        while (binNumber != 0) {
+            int digit = binNumber % 10;
+
+            if (digit != 1 && digit != 0) {
+                System.out.println("Number is not binary");
+                System.exit(0);
+            }
+
+            intNumber += digit * (int) Math.pow(2.0, exponent++);
+            binNumber /= 10;
+        }
+
+        System.out.println(intNumber);
+    }
 }
