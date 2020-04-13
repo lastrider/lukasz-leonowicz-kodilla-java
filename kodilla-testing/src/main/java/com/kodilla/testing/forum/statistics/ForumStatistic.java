aa6aca5dd75fd0fc2ae36417ace0aa93c6d7efd6
lastrider <l.leonowicz@gmail.com>
2020-04-13 23:09:16 +0200

    Task 6.5

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistic.java
@@ -0,0 +1,66 @@
+package com.kodilla.testing.forum.statistics;
+
+public class ForumStatistic {
+    Statistics statistics;
+    private int numberOfUsers;
+    private int numberOfPosts;
+    private int numberOfComments;
+    private double postsPerUsersRatio;
+    private double commentsPerUsersRatio;
+    private double commentsPerPostsRatio;
+
+    public int getNumberOfUsers() {
+        return numberOfUsers;
+    }
+
+    public int getNumberOfPosts() {
+        return numberOfPosts;
+    }
+
+    public int getNumberOfComments() {
+        return numberOfComments;
+    }
+
+    public double getPostsPerUsersRatio() {
+        return postsPerUsersRatio;
+    }
+
+    public double getCommentsPerUsersRatio() {
+        return commentsPerUsersRatio;
+    }
+
+    public double getCommentsPerPostsRatio() {
+        return commentsPerPostsRatio;
+    }
+
+    void calculateAdvStatistics(Statistics statistics) {
+        numberOfUsers = statistics.usersNames().size();
+        numberOfPosts = statistics.postsCount();
+        numberOfComments = statistics.commentsCount();
+
+        if (numberOfUsers <= 0) {
+            postsPerUsersRatio = 0.0;
+            commentsPerUsersRatio = 0.0;
+        } else {
+            postsPerUsersRatio = (double) numberOfPosts / numberOfUsers;
+            commentsPerUsersRatio = (double) numberOfComments / numberOfUsers;
+        }
+        if (numberOfPosts <= 0) {
+            commentsPerPostsRatio = 0.0;
+        } else {
+            commentsPerPostsRatio = (double) numberOfComments / numberOfPosts;
+        }
+    }
+
+    public String showStatistics() {
+        return "ForumStatistic{" +
+                "statistics=" + statistics +
+                ", numberOfUsers=" + numberOfUsers +
+                ", numberOfPosts=" + numberOfPosts +
+                ", numberOfComments=" + numberOfComments +
+                ", postsPerUserRatio=" + postsPerUsersRatio +
+                ", commentPerUserRatio=" + commentsPerUsersRatio +
+                ", commentsPerPostRatio=" + commentsPerPostsRatio +
+                '}';
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
@@ -0,0 +1,171 @@
+package com.kodilla.testing.forum.statistics;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ForumStatisticsTestSuite {
+
+    @Test
+    public void testZeroPostCounts() {
+        //Given
+        ForumStatistic forumStatistic = new ForumStatistic();
+        Statistics statistics = mock(Statistics.class);
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i <100 ; i++) {
+            list.add(String.valueOf(i));
+        }
+        when(statistics.usersNames()).thenReturn(list);
+        when(statistics.postsCount()).thenReturn(0);
+        when(statistics.commentsCount()).thenReturn(100);
+        //When
+        forumStatistic.calculateAdvStatistics(statistics);
+        double result1 = forumStatistic.getPostsPerUsersRatio();
+        double result2 = forumStatistic.getCommentsPerUsersRatio();
+        double result3 = forumStatistic.getCommentsPerPostsRatio();
+        //Then
+        Assert.assertEquals(0, result1, 0);
+        Assert.assertEquals(1, result2, 0);
+        Assert.assertEquals(0, result3, 0);
+    }
+
+    @Test
+    public void testManyPostCounts() {
+        //Given
+        ForumStatistic forumStatistic = new ForumStatistic();
+        Statistics statistics = mock(Statistics.class);
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i <100 ; i++) {
+            list.add(String.valueOf(i));
+        }
+        when(statistics.usersNames()).thenReturn(list);
+        when(statistics.postsCount()).thenReturn(1000);
+        when(statistics.commentsCount()).thenReturn(100);
+        //When
+        forumStatistic.calculateAdvStatistics(statistics);
+        double result1 = forumStatistic.getPostsPerUsersRatio();
+        double result2 = forumStatistic.getCommentsPerUsersRatio();
+        double result3 = forumStatistic.getCommentsPerPostsRatio();
+        //Then
+        Assert.assertEquals(10.0, result1, 0);
+        Assert.assertEquals(1.0, result2, 0);
+        Assert.assertEquals(0.1, result3, 0);
+    }
+
+    @Test
+    public void testZeroCommentsCounts() {
+        //Given
+        ForumStatistic forumStatistic = new ForumStatistic();
+        Statistics statistics = mock(Statistics.class);
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i <100 ; i++) {
+            list.add(String.valueOf(i));
+        }
+        when(statistics.usersNames()).thenReturn(list);
+        when(statistics.postsCount()).thenReturn(100);
+        when(statistics.commentsCount()).thenReturn(0);
+        //When
+        forumStatistic.calculateAdvStatistics(statistics);
+        double result1 = forumStatistic.getPostsPerUsersRatio();
+        double result2 = forumStatistic.getCommentsPerUsersRatio();
+        double result3 = forumStatistic.getCommentsPerPostsRatio();
+        //Then
+        Assert.assertEquals(1.0, result1, 0);
+        Assert.assertEquals(0.0, result2, 0);
+        Assert.assertEquals(0.0, result3, 0);
+    }
+
+    @Test
+    public void testMorePostThanComments() {
+        //Given
+        ForumStatistic forumStatistic = new ForumStatistic();
+        Statistics statistics = mock(Statistics.class);
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i <100 ; i++) {
+            list.add(String.valueOf(i));
+        }
+        when(statistics.usersNames()).thenReturn(list);
+        when(statistics.postsCount()).thenReturn(1000);
+        when(statistics.commentsCount()).thenReturn(100);
+        //When
+        forumStatistic.calculateAdvStatistics(statistics);
+        double result1 = forumStatistic.getPostsPerUsersRatio();
+        double result2 = forumStatistic.getCommentsPerUsersRatio();
+        double result3 = forumStatistic.getCommentsPerPostsRatio();
+        //Then
+        Assert.assertEquals(10.0, result1, 0);
+        Assert.assertEquals(1.0, result2, 0);
+        Assert.assertEquals(0.1, result3, 0);
+    }
+
+    @Test
+    public void testLessPostThanComments() {
+        //Given
+        ForumStatistic forumStatistic = new ForumStatistic();
+        Statistics statistics = mock(Statistics.class);
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i <100 ; i++) {
+            list.add(String.valueOf(i));
+        }
+        when(statistics.usersNames()).thenReturn(list);
+        when(statistics.postsCount()).thenReturn(100);
+        when(statistics.commentsCount()).thenReturn(1000);
+        //When
+        forumStatistic.calculateAdvStatistics(statistics);
+        double result1 = forumStatistic.getPostsPerUsersRatio();
+        double result2 = forumStatistic.getCommentsPerUsersRatio();
+        double result3 = forumStatistic.getCommentsPerPostsRatio();
+        //Then
+        Assert.assertEquals(1.0, result1, 0);
+        Assert.assertEquals(10.0, result2, 0);
+        Assert.assertEquals(10.0, result3, 0);
+    }
+
+    @Test
+    public void testZeroUsers() {
+        //Given
+        ForumStatistic forumStatistic = new ForumStatistic();
+        Statistics statistics = mock(Statistics.class);
+        List<String> list = new ArrayList<>();
+        when(statistics.usersNames()).thenReturn(list);
+        when(statistics.postsCount()).thenReturn(100);
+        when(statistics.commentsCount()).thenReturn(100);
+        //When
+        forumStatistic.calculateAdvStatistics(statistics);
+        double result1 = forumStatistic.getPostsPerUsersRatio();
+        double result2 = forumStatistic.getCommentsPerUsersRatio();
+        double result3 = forumStatistic.getCommentsPerPostsRatio();
+        //Then
+        Assert.assertEquals(0, result1, 0);
+        Assert.assertEquals(0, result2, 0);
+        Assert.assertEquals(1, result3, 0);
+    }
+
+    @Test
+    public void testAllEquallyAboveZero() {
+        //Given
+        ForumStatistic forumStatistic = new ForumStatistic();
+        Statistics statistics = mock(Statistics.class);
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i <100 ; i++) {
+            list.add(String.valueOf(i));
+        }
+        when(statistics.usersNames()).thenReturn(list);
+        when(statistics.postsCount()).thenReturn(100);
+        when(statistics.commentsCount()).thenReturn(100);
+        //When
+        forumStatistic.calculateAdvStatistics(statistics);
+        double result1 = forumStatistic.getPostsPerUsersRatio();
+        double result2 = forumStatistic.getCommentsPerUsersRatio();
+        double result3 = forumStatistic.getCommentsPerPostsRatio();
+        //Then
+        Assert.assertEquals(1, result1, 0);
+        Assert.assertEquals(1, result2, 0);
+        Assert.assertEquals(1, result3, 0);
+    }
+}
