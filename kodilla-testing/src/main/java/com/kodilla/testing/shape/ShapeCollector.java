bb75319c1d2c13b730b6a15aa9139dc69979f67d
lastrider <l.leonowicz@gmail.com>
2020-04-13 14:00:29 +0200

    Task 6.4 final

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -1,5 +1,4 @@
 package com.kodilla.testing.shape;
-
 import java.util.ArrayList;
 import java.util.List;
 
--- kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -1,5 +1,4 @@
 package com.kodilla.testing.shape;
-
 import org.junit.*;
 
 public class ShapeCollectorTestSuite {
