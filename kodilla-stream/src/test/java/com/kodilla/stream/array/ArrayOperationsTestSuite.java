cb7ff94808efb98e1fd71ac23e5097bc1fb1d00a
lastrider <l.leonowicz@gmail.com>
2020-04-21 14:21:25 +0200

    Task 7.5

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,18 @@
+package com.kodilla.stream.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+    public void testGetAverage(){
+        //Given
+        int [] array = new int[]{1,2,523,23,456,657,86,35,234,643,46,54,2,13,532,346,456,46,562,24,13};
+        //When
+        double result = 226.380952380952;
+        //Then
+        Assert.assertEquals(result,ArrayOperations.getAverage(array),0.0000000001);
+    }
+
+}
