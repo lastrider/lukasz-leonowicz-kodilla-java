ad3604f8fb8a088f25cce12087be94204bc3bd82
lastrider <62941695+lastrider@users.noreply.github.com>
2020-07-18 16:30:00 +0200

    All 2020-07-18

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/claculator/Calculator.java
@@ -0,0 +1,29 @@
+package com.kodilla.spring.claculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+
+    private final Display display;
+
+    public Calculator(Display display) {
+        this.display = display;
+    }
+
+    public double add(double a, double b) {
+        return display.displayValue(a + b);
+    }
+
+    public double div(double a, double b) {
+        return display.displayValue(a - b);
+    }
+
+    public double mul(double a, double b) {
+        return display.displayValue(a * b);
+    }
+
+    public double sub(double a, double b) {
+        return display.displayValue(a / b);
+    }
+}
